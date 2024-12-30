83d21a9584409fb93e3141e8dbe8b5b531bae988
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2024-12-30 10:11:17 +0000

    Fix

--- actions/generate-docker-build-matrix/src/utils.ts
+++
@@ -1,6 +0,0 @@
-import { context } from '@actions/github';
-import { Context } from '@actions/github/lib/context';
-
-export function getCommittedAt(context: Context) {
-  return new Date(context.payload.head_commit.timestamp).getTime() / 1000;
-}
